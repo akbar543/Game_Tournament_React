30a95eb24d201fd0d2e5ba452f442c35e73dd7ee
Manas Acharya <manas.acharya4131@gmail.com>
2023-04-13 21:06:01 +0530

    navbar, slider, body2, 13/04 , 21:05

--- src/components/HeroSlider.js
+++ src/components/HeroSlider.js
@@ -1,56 +1,74 @@
 import React from 'react'
-import { Carousel} from 'react-bootstrap';
+import { Carousel } from 'react-bootstrap';
 
 const HeroSlider = () => {
   return (
     <>
-      <Carousel>
-      <Carousel.Item interval={3000} style={{position:'relative'}}> 
-        <img
-          height={700}
-          className="d-block w-100"
-          src="https://images.pexels.com/photos/735911/pexels-photo-735911.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="First slide"
-        />
-        <div style={{position:'absolute', bottom:0, color:'white', height:400, marginLeft: 150}}>
+      <Carousel >
+        <Carousel.Item interval={3000} style={{ position: 'relative' } } >
+          <img
+            height={700}
+            className="d-block w-100"
+            src="https://themes.pixiesquad.com/pixiefreak/twisting-nether/wp-content/uploads/2018/10/slide-01.jpg"
+            alt="First slide"
+          />
+          <div style={{ position: 'absolute', bottom: 0, color: 'white', height: 400, marginLeft: 150 }}>
             <h1>Esports on Demand</h1>
             <p>Challenge players of your own skill level in your favorite games.</p>
             <p>Compete in daily tournaments and claim your share of prizes.</p>
-            <button>Register for Free</button>
-        </div>
-      </Carousel.Item>
+            <a href="/home" className='hero-button' target="_blank">
+              <span> </span>
+              <span> </span>
+              <span> </span>
+              <span> </span>
+              REGISTER FOR FREE
+            </a>
+          </div>
+        </Carousel.Item>
 
-      <Carousel.Item interval={3000}>
-        <img 
-          height={700}
-          className="d-block w-100"
-          src="https://images.pexels.com/photos/442576/pexels-photo-442576.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="Second slide"
-        />
-        <div style={{position:'absolute', bottom:0, color:'white', height:400, marginLeft: 150}}>
+        <Carousel.Item interval={3000}>
+          <img
+            height={700}
+            className="d-block w-100"
+            src="https://themes.pixiesquad.com/pixiefreak/twisting-nether/wp-content/uploads/2018/10/slide_01.jpg"
+            alt="Second slide"
+          />
+          <div style={{ position: 'absolute', bottom: 0, color: 'white', height: 400, marginLeft: 150 }}>
             <h1>Games</h1>
             <p>Play your favorite esports titles and crush your opponents in daily tournaments,
-exciting leagues and thrilling challenges.</p>
-            <button>Browse Games</button>
-        </div>
-      </Carousel.Item>
-      
-      <Carousel.Item interval={3000}>
-        <img
-        height={700}
-          className="d-block w-100"
-          src="https://images.pexels.com/photos/159369/xbox-xbox-one-microsoft-joystick-159369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="Third slide"
-        />
-        <div style={{position:'absolute', bottom:0, color:'white', height:400, marginLeft: 150}}>
+              exciting leagues and thrilling challenges.</p>
+            <a href="/home" className='hero-button' target="_blank">
+              <span> </span>
+              <span> </span>
+              <span> </span>
+              <span> </span>
+              Browse Games
+            </a>
+          </div>
+        </Carousel.Item>
+
+        <Carousel.Item interval={3000}>
+          <img
+            height={700}
+            className="d-block w-100"
+            src="https://themes.pixiesquad.com/pixiefreak/twisting-nether/wp-content/uploads/2018/10/slide_03.jpg"
+            alt="Third slide"
+          />
+          <div style={{ position: 'absolute', bottom: 0, color: 'white', height: 400, marginLeft: 150 }}>
             <h1>Treasure</h1>
             <p>Compete for prize money and other digital prizes - Team or Solo.</p>
-            <button>Join Epulze</button>
-        </div>
-      </Carousel.Item>
-    </Carousel>
+            <a href="/home" className='hero-button' target="_blank">
+              <span> </span>
+              <span> </span>
+              <span> </span>
+              <span> </span>
+              JOIN AKM
+              </a>
+          </div>
+        </Carousel.Item>
+      </Carousel>
     </>
-    );
+  );
 }
 
 export default HeroSlider
